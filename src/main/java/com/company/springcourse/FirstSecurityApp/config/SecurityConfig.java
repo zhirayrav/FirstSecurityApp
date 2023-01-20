86ca13fc1757700884c1a18736b4e1c6927f0a0c
Zhirayr Avanesyan <zhirayr.avanesyan@mail.ru>
2023-01-20 14:16:11 +0300

    Authorization config

--- src/main/java/com/company/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/company/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.company.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -23,6 +24,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(personDetailsService);
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests().antMatchers("/auth/login","/error").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/auth/login").loginProcessingUrl("/process_login")
+		.defaultSuccessUrl("/hello", true).failureUrl("/auth/login?error");
 	}
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
