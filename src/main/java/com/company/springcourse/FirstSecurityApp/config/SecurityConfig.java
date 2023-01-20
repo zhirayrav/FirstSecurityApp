bd878680d8d4a590610949624a7a5354acd4cb19
Zhirayr Avanesyan <zhirayr.avanesyan@mail.ru>
2023-01-20 12:35:21 +0300

    Auth(2) without AuthProviderImpl and using only PersonDetailService

--- src/main/java/com/company/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/company/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,25 +1,32 @@
 package com.company.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.company.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import com.company.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	private final AuthProviderImpl authProvider;
+	private final PersonDetailsService personDetailsService;
 	@Autowired
-	public SecurityConfig(AuthProviderImpl authProvider) {
+	public SecurityConfig(PersonDetailsService personDetailsService) {
 		super();
-		this.authProvider = authProvider;
+		this.personDetailsService = personDetailsService;
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) {
-		auth.authenticationProvider(authProvider);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(personDetailsService);
+	}
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 
